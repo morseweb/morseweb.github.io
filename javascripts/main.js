ea7ae7e2a2ed3e966bf02f7857ed4af1ea12e5c4
morseweb <marcal+morseweb@martinezrodrigo.com>
2014-05-21 22:57:02 -0400

    Add JS to change color

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
 console.log('This would be the main JS file.');
+
+function makeColork(color) {
+  for (var i in document.styleSheets[0].rules) {
+    var rule  = document.styleSheets[0].rules[i];
+    if("#main_content_wrap" == rule.selectorText) {
+      rule.style.background = color;
+      break;
+    }
+  }
+}
+
+function makeBlack() {
+  makeColor("rgb(55, 55, 55)");
+}
+
+function makeWhite() {
+  makeColor("rgb(242, 242, 242)");
+}
