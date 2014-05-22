e23875dd0e302f04c1340c87e087aa1de4edefd6
morseweb <marcal+morseweb@martinezrodrigo.com>
2014-05-21 23:00:35 -0400

    Fix typo

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,6 @@
 console.log('This would be the main JS file.');
 
-function makeColork(color) {
+function makeColor(color) {
   for (var i in document.styleSheets[0].rules) {
     var rule  = document.styleSheets[0].rules[i];
     if("#main_content_wrap" == rule.selectorText) {
